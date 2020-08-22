39c174733846552f24990601e96992e01647b58c
Sakshi Jain <sakshi172015@gmail.com>
2020-08-22 13:58:55 +0530

    width height set

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -4,18 +4,21 @@ import './Home.css'
 import { NavLink } from "react-router-dom";
 const Card = (props) =>{
 
-return(
-      <div className="col-md-4 col-10 mx-auto">
-           <div className="card ">
-              <img className="living_room" src={props.imgsrc} className="card-img-top" alt={props.imgsrc}/>
-                <div className="card-body">
-                  <h5 className="card-title font-weight-bold">{props.title}</h5>
-                   <p className="card-text">{props.body}</p>
-                   <NavLink to="" className="btn btn-primary">Go somewhere</NavLink>
-                 </div>
+  const onClickHandler = () => {
+    console.log("clicked")
+  }
+  return(
+        <div className="col-md-4 col-10 mx-auto">
+          <div className="card ">
+            <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc}/>
+            <div className="card-body">
+              <h5 className="card-title font-weight-bold">{props.title}</h5>
+              <p className="card-text">{props.body}</p>
+              <NavLink to="" className="btn btn-primary" onClick={onClickHandler}>More Details</NavLink>
             </div>
-           </div>
-);
+          </div>
+        </div>
+  );
 
 };
 
