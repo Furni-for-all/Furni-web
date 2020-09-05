9985678c4ecc248d88049cae4e17927f430bb5a7
Nandini Bagga <bagganandini8@gmail.com>
2020-09-05 18:14:28 +0530

    minor card details

--- src/components/MoreDetailCard/MoreDetailCard.jsx
+++ src/components/MoreDetailCard/MoreDetailCard.jsx
@@ -5,11 +5,11 @@ import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 function MoreDetailCard(props) {
     return (
         <div className="col-md-4 col-10 mx-auto">
-          <div className="card ">
-            <img src={props.imgsrc} className="card-img-top" alt={props.imgsrc}/>
-            <div className="card-body">
-              <h5 className="card-title font-weight-bold">{props.title}</h5>
-              <p className="card-text">{props.body}</p>               
+          <div className="detail-card">
+            <img src={props.imgsrc} className="detail-card-img-top" alt={props.imgsrc}/>
+            <div className="detail-card-body">
+              <h5 className="detail-card-title font-weight-bold">{props.title}</h5>
+              <p className="detail-card-text">{props.body}</p>               
             </div>
             <a href="http://www.google.com"><DoubleArrowIcon className="amazon-icon"/></a>
           </div>
